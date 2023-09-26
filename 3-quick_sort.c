9f47cb16fb4f40d423af728d70ec2f4a6d03ae66
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-09-26 12:26:30 -0700

    Task3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,30 +12,30 @@
 size_t lomuto_partition(int *array, size_t size, int low, int high)
 {
 	int pivot = array[high];
-	int temp, j, i = low - 1;
+	int temp;
+	int j, i = low - 1;
+	int swap_occurred = 0;
 
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] < pivot)
 		{
 			i++;
-			if (i != j)
-			{
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			swap_occurred = 1;
 		}
 	}
 
-	if ((i + 1) != high)
-	{
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
+	/* Swap the pivot element to its correct position */
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	swap_occurred = 1;
+
+	if (swap_occurred)
 		print_array(array, size);
-	}
 
 	return (i + 1);
 }
